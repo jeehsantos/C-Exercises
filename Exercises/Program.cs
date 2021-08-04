f938f1a6b577979d4a4030c5e47a7d5f55a2b872
Jefferson Macedo <jeehsantos@hotmail.com.br>
2021-08-04 23:57:44 +1200

    Add project files.

---
+++ Exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            p1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p1.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: "+ p1);
+
+            Console.WriteLine("Digite o numero de produtos a ser adicionado ao estoque: ");
+            p1.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados atualizados: "+ p1);
+
+            Console.WriteLine("Digite o numero de produtos a ser removido ao estoque: ");
+            p1.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados atualizados: " + p1);
+
+        }
+
+
+
+    }
+}
---
+++ Exercises02/Retangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises02
+{
+    public class Retangulo
+    {
+        double Largura;
+        double Altura;
+
+        public double Area() { }
+        public double Perimetro() { }
+        public double Diagonal() { }
+    }
+}
