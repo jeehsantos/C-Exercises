a8507e7a49b887e0b4b113a0aa4279c88dea4b44
Jefferson Macedo <jeehsantos@hotmail.com.br>
2021-08-05 21:53:31 +1200

    Encapsulation exercise
    Encapsulation exercise using deposit and withdraw from a bank account

---
+++ Exercicio05/ContaBancaria.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio05
+{
+    class ContaBancaria
+    {
+        private string _nome;
+        public int Conta { get; set; }
+        public double Saldo { get; private set; }
+        
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length >= 3)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public ContaBancaria()
+        {
+        }
+        public ContaBancaria(string nome, int conta)
+        {
+            Nome = nome;
+            Conta = conta;
+        }
+
+        public Boolean CheckDeposit(string valid)
+        {
+            if (valid.Equals("s"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Deposit(double value)
+        {
+            if(value > 0)
+            {
+                Saldo += value;
+            }
+        }
+
+        public double Withdraw(double value)
+        {
+            return  Saldo -= (value + 5.00);
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                   + Conta
+                   + ", Titular :"
+                   + Nome
+                   + ", Saldo $ "
+                   + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ Exercicio05/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Exercicio05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o Titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Havera deposito inicial (s/n) ? ");
+            string deposito = Console.ReadLine();
+            ContaBancaria conta = new ContaBancaria(nome, numero);
+
+            if (conta.CheckDeposit(deposito))
+            {
+                Console.Write("Entre o valor de deposito inicial $ ");
+                conta.Deposit(double.Parse(Console.ReadLine()));
+                Console.WriteLine("");
+                Console.WriteLine("Dados da conta: ");
+                Console.WriteLine(conta);
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Dados da conta: ");
+                Console.WriteLine(conta);
+            }
+
+            Console.WriteLine("");
+            Console.Write("Entre um valor para deposito $ ");
+            conta.Deposit(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("");
+            Console.Write("Entre um valor para saque $ ");
+            conta.Withdraw(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+        }
+    }
+}
