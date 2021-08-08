bdc4f6b916af1a7d9557718a725defb549ccd746
Jefferson Macedo <jeehsantos@hotmail.com.br>
2021-08-08 20:51:29 +1200

    matrix exercise08

---
+++ Exercise08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int negatives = 0;
+            int[,] mat = new int[x,x];
+
+            for(int n = 0; n < x; n++) {
+                string[] newNumber = Console.ReadLine().Split(' ');
+                for (int i = 0; i < x; i++)
+                {
+                    if (int.Parse(newNumber[i]) < 0)
+                    {
+                        negatives++;
+                    }
+                    mat[n, i] = int.Parse(newNumber[i]);
+                }
+            }
+
+            Console.WriteLine("Main diagonal");
+            for (int n = 0; n < x; n++)
+            {
+                    Console.WriteLine(mat[n,n]);
+                
+            }
+            Console.Write("Negative numbers: "+ negatives);
+        }
+    }
+}
