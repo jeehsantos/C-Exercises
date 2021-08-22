13cc2d10bd490391e504fa943128aa635d648b54
Jefferson Macedo <jeehsantos@hotmail.com.br>
2021-08-22 19:47:17 +1200

    Exercise20
    Exercise using Generic concept and IComparable

---
+++ Exercise20/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+ 
+
+namespace Exercise20
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", - " + Price.ToString("F2");
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: Argument is not a product");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ Exercise20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> lista = new List<Product>();
+           
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                string[] product =  Console.ReadLine().Split(',');
+                lista.Add(new Product(product[0], double.Parse(product[1])));
+            }
+
+            CalculationService newCalc = new CalculationService();
+            Product max = newCalc.Max(lista);
+
+            Console.WriteLine("Most expensive: ");
+            Console.WriteLine(max);
+        }
+    }
+}
